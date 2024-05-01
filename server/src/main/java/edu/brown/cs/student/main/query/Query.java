dc26c3ccbd3ffbd1d5db7010935ee57448949d7d
Candice Cai <caixy.candice2016@outlook.com>
2024-04-30 21:03:54 -0400

    refractor: deleted unnecessary files

--- server/src/main/java/edu/brown/cs/student/main/query/Query.java
+++
@@ -1,9 +0,0 @@
-package edu.brown.cs.student.main.query;
-
-/**
- * Abstract class whos contains() method is implemented by classes defining various query methods
- */
-public abstract class Query {
-
-  public abstract boolean contains(String[] row);
-}
--- server/src/main/java/edu/brown/cs/student/main/server/Server.java
+++ server/src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -43,34 +43,6 @@ public class Server {
           response.header("Access-Control-Allow-Credentials", "true");
         });
 
-    // StorageInterface firebaseUtils;
-    // // CSVSharedVar csvSharedVar = new CSVSharedVar();
-    // try {
-    //   firebaseUtils = new FirebaseUtilities();
-    // Setting up the handler for the GET /order and /activity endpoints
-    // Spark.get("add-pin", new AddPinHandler(firebaseUtils));
-    // Spark.get("list-pins", new ListPinsHandler(firebaseUtils));
-    // Spark.get("rmv-pin", new RemovePinHandler(firebaseUtils));
-    // Spark.get("clear-user", new ClearUserHandler(firebaseUtils));
-    // GeoSearcher geoSearcher = new GeoSearcher();
-    // CachedGeo cachedGeo = new CachedGeo(geoSearcher, 100, 60);
-    // Spark.get("geoquery", new GeoJSONHandler(cachedGeo));
-    // Spark.get("areaquery", new SearchAreaHandler());
-    // Spark.get("loadcsv", new LoadCSVHandler(csvSharedVar));
-    // Spark.get("viewcsv", new ViewCSVHandler(csvSharedVar));
-    // Spark.get("searchcsv", new SearchCSVHandler(csvSharedVar));
-    // ACSSearcher acsSearcher =
-    //     new ACSSearcher(); // Assuming default constructor, adjust as necessary
-    // CachedACSInfo cachedACSInfo =
-    //     new CachedACSInfo(acsSearcher, 100, 60); // Adjust parameters as necessary
-    // // Spark.get("broadband", new BroadbandHandler());
-    // Spark.get("broadband", new BroadbandHandler(cachedACSInfo));
-    // Spark.notFound(
-    //     (request, response) -> {
-    //       response.status(404); // Not Found
-    //       System.out.println("ERROR");
-    //       return "404 Not Found - The requested endpoint does not exist.";
-    //     });
     try {
       FirebaseUtilities firebaseUtils = new FirebaseUtilities();
       Spark.get("/getUserProfile", new GetUserProfileHandler(firebaseUtils));
