f4f293e507124871f6eaf55aa9f911ad5fb0ab5a
JazJaz426 <jiaming_lin@brown.edu>
2024-05-22 15:09:40 -0400

    fix: fix server 500 internal error bug

--- server/src/main/java/edu/brown/cs/student/main/server/handlers/UpdateUserProfileHandler.java
+++ server/src/main/java/edu/brown/cs/student/main/server/handlers/UpdateUserProfileHandler.java
@@ -1,7 +1,6 @@
 package edu.brown.cs.student.main.server.handlers;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseToken;
+import com.google.gson.Gson;
 import edu.brown.cs.student.main.server.storage.StorageInterface;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,28 +17,25 @@ public class UpdateUserProfileHandler implements Route {
 
   @Override
   public Object handle(Request request, Response response) throws Exception {
-    String token = request.headers("Authorization").substring(7); // Assuming Bearer token
-    FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
-    String userEmail = decodedToken.getEmail();
-
+    String body = request.body();
+    Gson gson = new Gson();
+    Map<String, String> requestBody = gson.fromJson(body, Map.class);
     Map<String, Object> responseMap = new HashMap<>();
+    String email = requestBody.get("email");
     try {
-      String newName = request.queryParams("name");
+      String newName = requestBody.get("name");
       if (newName == null) {
         responseMap.put("status", 400);
         responseMap.put("message", "User name is required.");
         return Utils.toMoshiJson(responseMap);
       }
-      String newAddress = request.queryParams("address");
+      String newAddress = requestBody.get("address");
       if (newAddress == null) {
         responseMap.put("status", 400);
         responseMap.put("message", "User address is required.");
         return Utils.toMoshiJson(responseMap);
       }
-      System.out.println("userEmail: " + userEmail);
-      System.out.println("new Name: " + newName);
-      System.out.println("new Address: " + newAddress);
-      firebaseUtilities.updateUserProfile(userEmail, newName, newAddress);
+      firebaseUtilities.updateUserProfile(email, newName, newAddress);
       responseMap.put("status", 200);
       responseMap.put("message", "User profile updated successfully.");
     } catch (Exception e) {
